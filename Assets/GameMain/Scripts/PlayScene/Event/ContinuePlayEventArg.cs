190705151b1ffe210ab4d748b130ddc13c794202
MoCastle <18224837254@163.com>
2019-09-20 08:56:51 +0800

    进度推进

---
+++ Assets/GameMain/Scripts/PlayScene/Event/ContinuePlayEventArg.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FrameWork;
+
+namespace GameProject.PlayScene
+{
+    public class ContinuePlayEventArg : FrameWorkEventArg
+    {
+    }
+}
---
+++ Assets/GameMain/Scripts/PlayScene/UI/PlaySceneUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameProject.PlayScene;
+using FrameWork;
+
+namespace GameProject{
+	public class PlaySceneUI : MonoBehaviour {
+        [SerializeField]
+        GameObject clickCheck;
+        private void Awake()
+        {
+            //GameControler.singleton.eventManager.RegistEvent<ContinuePlayEventArg>(OnPlayContinue);
+        }
+        public void OnPlayContinue(object sender,FrameWorkEventArg arg)
+        {
+           // clickCheck.active = false;
+        }
+        public void OnMouseClick()
+        {
+            GameControler.singleton.eventManager.FireEvent<MouseClickEventArg>(this,null);
+            clickCheck.active = false;
+        }
+	}
+}
